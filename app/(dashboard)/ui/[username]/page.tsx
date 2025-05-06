5105d636445575401fe5d9dbdaad95a0404f6795
gauravPanditr <gauravpandit7856@gmail.com>
2025-05-06 09:47:56 +0530

    created dashboard

--- app/(dashboard)/ui/[username]/page.tsx
+++ app/(dashboard)/ui/[username]/page.tsx
@@ -1,9 +1,9 @@
 import { notFound } from 'next/navigation'
 
 import getUserByUsername from '@/lib/user-service'
-import { isFollowingUser } from '@/lib/follow-service'
+//import { isFollowingUser } from '@/lib/follow-service'
 import { isBlockedByUser } from '@/lib/block-service'
-import { StreamPlayer } from '@/components/stream-player'
+//import { StreamPlayer } from '@/components/stream-player'
 
 interface UserPageProps {
   params: {
@@ -18,15 +18,15 @@ const UserPage = async ({ params }: UserPageProps) => {
     notFound()
   }
 
-  const isFollowing = await isFollowingUser(user.id)
+  //const isFollowing = await isFollowingUser(user.id)
   const isBlocked = await isBlockedByUser(user.id)
 
   if (isBlocked) {
     notFound()
   }
 
-  return (
-    <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing} />
-  )
+  // return (
+  //   <StreamPlayer user={user} stream={user.stream} isFollowing={isFollowing} />
+  // )
 }
 export default UserPage
